b3bb97994c415035b790bc913fd5671ab2c44461
b.kohnen@gmail.com <b.kohnen@gmail.com@57afc76f-2316-4ef3-38a1-206d6fca4a00>
2011-02-23 08:24:17 +0000

    Einige Comments verfasst

--- trunk/Workspace/Encuesta-Projekt/app/controllers/surveytypes_controller.rb
+++ trunk/Workspace/Encuesta-Projekt/app/controllers/surveytypes_controller.rb
@@ -3,13 +3,15 @@ class SurveytypesController < ApplicationController
   	@surveytype = Surveytype.new
     @title = "Erstellen von neuem Umfragetyp"
   end
-
+ #nicht vorgesehen, da eine Änderung das gesamte System durcheinander bringen wird!!
   def edit
   end
 
+  # Entfernen eines Umfragrypen
   def delete
   end
   
+  #Erstellen eines neunen Umfragetypen (Simple und normal sind standard)
   def create
   	 @surveytype = Surveytype.new(params[:surveytype])
   end
