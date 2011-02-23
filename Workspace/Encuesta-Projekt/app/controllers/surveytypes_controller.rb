14764c7f1438be3b9a24e7c280ee903fdaed186b
b.kohnen <b.kohnen@gmail.com>
2011-02-23 08:25:12 +0000

    -

--- Workspace/Encuesta-Projekt/app/controllers/surveytypes_controller.rb
+++ Workspace/Encuesta-Projekt/app/controllers/surveytypes_controller.rb
@@ -3,12 +3,14 @@ class SurveytypesController < ApplicationController
   	@surveytype = Surveytype.new
     @title = "Erstellen von neuem Umfragetyp"
   end
+  
  #nicht vorgesehen, da eine Änderung das gesamte System durcheinander bringen wird!!
   def edit
   end
 
   # Entfernen eines Umfragrypen
   def delete
+  	
   end
   
   #Erstellen eines neunen Umfragetypen (Simple und normal sind standard)
